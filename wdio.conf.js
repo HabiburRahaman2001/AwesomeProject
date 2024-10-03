73bb0545353c0185a89f800fd4b44eb91d68344c
Apurvaroy2001 <hrahaman2001@gmail.com>
2024-10-03 12:45:27 +0530

    Appium Test

--- wdio.conf.js
+++ wdio.conf.js
@@ -306,9 +306,11 @@
 
 exports.config = {
     services: ['appium'],
-    hostname: process.env.APPIUM_SERVER || 'localhost', // Use Ngrok URL for remote access
-    // hostname: '0abb-2401-4900-1c01-6e03-481e-ed7d-6773-a8e0.ngrok-free.app', // Use Ngrok URL for remote access
-    port: process.env.APPIUM_PORT || 4723, // Optional: Use environment variable for port
+    // hostname: process.env.APPIUM_SERVER || 'localhost', // Use Ngrok URL for remote access
+    hostname: '0abb-2401-4900-1c01-6e03-481e-ed7d-6773-a8e0.ngrok-free.app', // Use Ngrok URL for remote access
+    // port: process.env.APPIUM_PORT || 4723, // Optional: Use environment variable for port
+    port: 4723, // Optional: Use environment variable for port
+
     path: '/wd/hub',
     protocol: 'http', // Use 'http' if running Appium locally; switch to 'https' for Ngrok
     runner: 'local',
